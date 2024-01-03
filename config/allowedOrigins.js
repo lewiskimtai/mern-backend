3f2bcdfb5d7f761764ecc349b79b0f25212f684e
lewiskimtai <lewiskimtai@gmail.com>
2024-01-03 21:49:14 +0300

    added allowed cors sites

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://analytics-dash.onrender.com",
+];
 
 export default allowedOrigins;
